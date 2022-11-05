929a8f9decc7e836ae178ff9a370843e19e9371f
Vinh <73513296+VinnyBui@users.noreply.github.com>
2022-11-05 03:42:57 -0700

    setup mainpage layout. Also add custom color classes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./app/**/*.hbs"],
   theme: {
+    colors: {
+      //color palette
+      'green':{
+        100:'#cad2c5',
+        200:'#84a98c',
+        300:'#52796f',
+        400:'#354f52',
+        500:'#2f3e46',
+      },
+      'gray':'#e5e5e5',
+      'white':'#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
