7a44ef6ded223a98198cbe50b322095590e31885
Vinh <73513296+VinnyBui@users.noreply.github.com>
2022-11-08 21:32:51 -0800

    remove drk theme. Adjusted nav bar and diary entries sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.hbs', './node_modules/tw-elements/dist/js/**/*.js'],
+  daisyui: {
+    themes: false,
+  },
   theme: {
-    colors: {
-      //color palette
-      'green':{
-        100:'#cad2c5',
-        200:'#84a98c',
-        300:'#52796f',
-        400:'#354f52',
-        500:'#2f3e46',
+    extend: {
+      colors: {
+        //color palette
+        'green':{
+          100:'#cad2c5',
+          200:'#84a98c',
+          300:'#52796f',
+          400:'#354f52',
+          500:'#2f3e46',
+        },
+        'gray':'#e5e5e5',
+        'white':'#FFFFFF',
       },
-      'gray':'#e5e5e5',
-      'white':'#FFFFFF',
     },
-    extend: {},
   },
   plugins: [
     require('tw-elements/dist/plugin'),
